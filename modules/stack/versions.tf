8e088746fedc08e4106f73320eb10b0f305249c1
Roman Orlovskiy <roman.orlovskiy@celsius.network>
2022-04-01 22:26:14 +0300

    adding creation of separate IAM roles in each stack when using shared workers

--- modules/stack/versions.tf
+++ modules/stack/versions.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
       version = ">= 0.1.7"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.32"
     }
   }
 }
